3803f24470fe84a99a9cc8dc00e9027b3cf8b02e
Sena <dimatesebas73@gmail.com>
2024-10-30 17:51:21 -0500

    last progress

--- Countries - Adso-722/scripts/app.js
+++ Countries - Adso-722/scripts/app.js
@@ -1,4 +1,15 @@
 const makeCountry = (country) => {
     const container = document.createElement ('div')
     container.classList.add('card-country')
+    const imgContainer = document.createElement('img')
+    imgContainer.src = country.flag
+    imgContainer.alt = country.name
+
+    const nameContainer = document.createElement('h2')
+    
+
+    container.appendChild(imgContainer)
+    container.appendChild(nameContainer)
+
+    document.querySelector('main > section').appendChild (container)
 }
